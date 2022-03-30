00eb038a95e4202161548795ffb06e1523a1f697
Dhruba Jyoti Seal <dhrubaseal@gmail.com>
2022-03-30 22:44:31 +0530

    new1

--- Arrays.py
+++ Arrays.py
@@ -1,35 +1,12 @@
-#Let us say your expense for every month are listed below,
-#January - 2200
-#February - 2350
-#March - 2600
-#April - 2130
-#May - 2190
+#You have a list of your favourite marvel super heros
 
-#Create a list to store these monthly expenses
+heroes = ['Spider man','Thor','Hulk','Iron man','Captain America' ]
 
-exp = [2200,2350,2600,2130,2190]
+#Length of the list
 
-#In Feb, how many dollars you spent extra compare to January?
+print(len(heroes))
 
-print(exp[1]-exp[0])
+#Add 'black panther' at the end of this list
 
-#Find out your total expense in first quarter (first three months) of the year.
-
-print(exp[0]+exp[1]+exp[2])
-
-#Find out if you spent exactly 2000 dollars in any month
-
-print(2000 in exp)
-
-#June month just finished and your expense is 1980 dollar. Add this item to our monthly expense list
-
-exp.append(1980)
-
-print(exp)
-
-# You returned an item that you bought in a month of April and got a refund of 200$. Make a correction to your
-# monthly expense list based on this
-
-exp[3] = exp[3] - 200
-
-print(exp)
+heroes.append('Black panther')
+print(heroes)
