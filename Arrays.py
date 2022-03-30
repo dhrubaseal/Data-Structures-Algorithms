c77b6fab8381312f36410035d2c3a231066530d4
Dhruba Jyoti Seal <dhrubaseal@gmail.com>
2022-03-30 22:51:29 +0530

    new1

--- Arrays.py
+++ Arrays.py
@@ -10,3 +10,20 @@ print(len(heroes))
 
 heroes.append('Black panther')
 print(heroes)
+
+#You realize that you need to add 'black panther' after 'hulk',
+#so remove it from the list first and then add it after 'hulk'
+
+heroes.remove('Black panther')
+heroes.insert(3,'Black panther')
+print(heroes)
+
+#Remove thor and hulk from list and replace them with doctor strang in one line of code
+
+heroes[1:3]=['Doctor strange']
+print(heroes)
+
+#Sort the list in alphabetical order
+
+heroes.sort()
+print(heroes)
