d2213ad51d99861b33d36112bbe4799a8be340e5
Dhruba Jyoti Seal <dhrubaseal@gmail.com>
2022-04-02 00:19:19 +0530

    new1

---
+++ Linked List/Linked List.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def insert_at_begining(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def print(self):
+        if self.head is None:
+            print("Linked List si empty")
+            return
+
+
+
+
+
+
+if __name__=='__main__':
+    pass
